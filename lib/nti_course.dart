431cb987907f01be36cd5189fa825e9242138221
mahmod-farid <mahmoudfarid.appdeveloper@gmail.com>
2025-04-25 21:28:30 +0300

    add center text in home

--- lib/nti_course.dart
+++ lib/nti_course.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('hello GitHup'),
+          child: Text('hello GitHub'),
         ),
       ),
     );
